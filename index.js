1612cf3f7b9b58e019a818996d17d6db86242f5d
charlieadam1903 <charlotte.adam@students.mq.edu.au>
2024-05-29 12:53:00 +1000

    added post command for app

--- index.js
+++ index.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express(); //create app
 
+app.use(express.json()); //add capability to understand json data 
+//coming in to our server, if its json content type it will parse it and make available to us
+const data = {
+    likes: [],
+};
+
 app.get('/', (req, res) => {
 res.status(418).send("I'm a teapot"); //associate function w/ request coming in for url and calling that function
 });
 
-app.get('/foo', (req, res) => {
-    res.json({hello: 'foo'}); //associate function w/ request coming in for url and calling that function
-    });
+app.get('/likes', (req, res) => {
+    res.json(data.likes); //associate function w/ request coming in for url and calling that function
+});
 
+app.post('/likes', (req, res) => { //sends post request
+    //can use tool called </> rested client but only for firefox i guess
+    const body = req.body;
+    if('thing' in req.body) {
+        data.likes.push(req.body.thing);
+        res.send({status:'success'});
+    }   else {
+        res.send({status: 'error', message: 'missing thing'});
+    } //now when posted it adds things you put in
+    console.log(body);
+    res.send('OK');
+});
 
 const port = 3123; 
 
